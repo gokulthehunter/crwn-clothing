a7b9a04cc688860fdb35948ca3d06bde1036eef4
admin <admin@admins-MacBook-Pro.local>
2020-01-14 11:30:20 +0530

    shop and collection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Switch,Route,Link} from 'react-router-dom';
 import './App.scss';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <nav>
+        <Link to="/shop">Shop</Link>
+      </nav>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
+      
     </div>
   );
 }
