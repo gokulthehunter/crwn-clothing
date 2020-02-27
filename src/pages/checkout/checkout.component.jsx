73482216e86f0f8098ec33009362cb9a44ca86be
admin <admin@admins-MacBook-Pro.local>
2020-02-27 11:40:00 +0530

    checkout-item add increase decrease func added to quantity

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -11,8 +11,6 @@ import {selectCartItems,selectCartTotal} from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
 // import CartItem from '../../components/cart-item/cart-item.component';
 
-
-
 const CheckoutPage = ({cartItems,total}) => (
     <div className="checkout-page">
         <div className="checkout-header">
