2c17a5a917e5c51660dbc12260a216440acb4caf
admin <admin@admins-MacBook-Pro.local>
2020-03-11 11:43:41 +0530

    collection page created

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import {BrowserRouter} from 'react-router-dom';
 
 
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react'
+
+import {store,persistor} from './redux/store';
 
 import './index.scss';
 import App from './App';
@@ -13,6 +15,8 @@ import App from './App';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>, document.getElementById('root'));
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render(){
-        const {collections} = this.state;
-        return(
-            <div className="shop-page">
-                {collections.map( ({id, ...otherProps})=>(
-                    <CollectionPreview key={id} {...otherProps}/>
-                ))}
-            </div>
-        )
-    }
-}
+import {Route} from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const ShopPage = ({match}) =>(
+    <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+        {/* <CollectionsOverview/> */}
+    </div>
+)
+
 
 export default ShopPage;
